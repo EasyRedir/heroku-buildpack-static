ab9771d87d4145ae9060d6a7a034349ff468572a
William Richards <william@deap.ca>
2017-01-01 19:27:49 -0700

    Added support for Nginx index directive

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -8,6 +8,7 @@ class NginxConfig
     encoding: "UTF-8",
     clean_urls: false,
     https_only: false,
+    index: false,
     worker_connections: 512
   }
 
@@ -33,6 +34,7 @@ class NginxConfig
 
     json["clean_urls"] ||= DEFAULT[:clean_urls]
     json["https_only"] ||= DEFAULT[:https_only]
+    json["index"] ||= DEFAULT[:index]
 
     json["routes"] ||= {}
     json["routes"] = NginxConfigUtil.parse_routes(json["routes"])
